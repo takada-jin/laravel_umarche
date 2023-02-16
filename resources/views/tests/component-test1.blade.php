dd4deb3b505b5fd0fbd85c559bca984dc2b0e3b6
takada.jin <>
2023-02-16 13:29:03 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,5 +3,6 @@
     1111
     <x-tests.card title="タイトル" content="本文" :message="$message"></x-tests.card>
     <x-tests.card title="タイトル"></x-tests.card>
+    <x-tests.card title="CSS変更" class="bg-red-300"></x-tests.card>
 
 </x-tests.app>
