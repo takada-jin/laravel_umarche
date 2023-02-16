855f1463540660a7aa3de4d3d5c89bb3d0823770
takada.jin <>
2023-02-16 14:00:51 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,7 @@
 <x-tests.app>
+    <x-slot name="header">へっだー</x-slot>
     2222
+    <x-test-class-base classBaseMessage="メッセージです"></x-test-class-base>
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています"></x-test-class-base>
 </x-tests.app>
