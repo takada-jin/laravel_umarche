0142e6e530f3083079e686b75a0b40c72d03fdc0
takada.jin <>
2023-02-16 15:05:18 +0900

    サービスコンテナ

---
+++ app/Http/Controllers/lifeCycleTestController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class lifeCycleTestController extends Controller
+{
+    //
+    public function showServiceContainerTest(){
+        app()->bind('lifeCycleTest',function(){
+            return 'ライフサイクルのテスト';
+        });
+        $test = app()->make('lifeCycleTest');
+        //サービスコンテナなし
+        // $message = new Message();
+        // $sample = new sumple($message);
+        // $sample->run();
+        //サービスコンテナあり
+        app()->bind('sample', Sample::class);
+        $sample = app()->make('sample');
+        $sample->run();
+        dd($test,app());
+    }
+}
+
+class Sample
+{
+    public $message;
+    public function __construct(Message $message){
+        $this->message = $message;
+    }
+    public function run(){
+        $this->message->send();
+    }
+}
+class Message
+{
+    public function send(){
+        echo('メッセージ表示');
+    }
+}
