12cf8e0d72626c0187984bba98150834fc41e5f1
takada.jin <>
2023-02-16 15:36:47 +0900

    サービスプロバイダのテスト

--- app/Http/Controllers/lifeCycleTestController.php
+++ app/Http/Controllers/lifeCycleTestController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class lifeCycleTestController extends Controller
 {
     //
+    public function showServiceProviderTest(){
+        $encrypt = app()->make('encrypter');
+        $password = $encrypt->encrypt('password');
+        $sample = app()->make('serviceProviderTest');
+        dd($sample, $password, $encrypt->decrypt($password));
+    }
     public function showServiceContainerTest(){
         app()->bind('lifeCycleTest',function(){
             return 'ライフサイクルのテスト';
