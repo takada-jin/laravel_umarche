c900b67c44488355a8ec95dfba5b994186810aee
takada.jin <>
2023-02-16 13:21:22 +0900

    props

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,5 +2,6 @@
     <x-slot name="header">ヘッダー1</x-slot>
     1111
     <x-tests.card title="タイトル" content="本文" :message="$message"></x-tests.card>
+    <x-tests.card title="タイトル"></x-tests.card>
 
 </x-tests.app>
