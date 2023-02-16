33b8213f97a1e55061014aca131dd51f638a770a
takada.jin <>
2023-02-16 13:14:53 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,6 +1,6 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー1</x-slot>
     1111
-    <x-tests.card title="タイトル" content="本文"></x-tests.card>
+    <x-tests.card title="タイトル" content="本文" :message="$message"></x-tests.card>
 
 </x-tests.app>
