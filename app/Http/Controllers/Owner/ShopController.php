1de6836b0ef6df9088e6a77c343de4a1bb8466f5
takada.jin <>
2023-02-23 15:23:16 +0900

    middleware, abort(404)

---
+++ app/Http/Controllers/Owner/ShopController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Owner;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Shop;
+
+use function PHPUnit\Framework\isNull;
+
+class ShopController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+            $id = $request->route()->parameter('shop');
+            if(!is_null($id)) {
+                $shopsOwnerId = Shop::findOrFail($id)->owner->id;
+                $shopId = (int)$shopsOwnerId;
+                $ownerId = Auth::id();
+                if($ownerId !== $shopId){
+                    abort(404);
+                }
+            }
+
+            return $next($request);
+        });
+    }
+
+    public function index()
+    {
+        $ownerId = Auth::id();
+        $shops = Shop::where('owner_id', $ownerId)->get();
+
+        return view('owner.shops.index',compact('shops'));
+    }
+
+    public function edit($id)
+    {
+        dd(Shop::findOrFail($id));
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+}
